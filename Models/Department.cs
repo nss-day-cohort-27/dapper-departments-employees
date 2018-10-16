e011faa21457fa2d76dbf8d54ad8146077e42281
Steve Brownlee <chortlehoort@gmail.com>
2018-10-16 10:54:04 -0500

    Initial commit

---
+++ Models/Department.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace DapperDepartments.Models
+{
+    // C# representation of the Department table
+    public class Department
+    {
+        public int Id { get; set; }
+
+        public string DeptName { get; set; }
+    }
+}
